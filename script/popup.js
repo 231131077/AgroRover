a2af8a75eddcab33b262bf89fad3a599f79d9ee5
Caprian Revaldo Sitepu <caprian.revaldo@students.mikroskil.ac.id>
2024-06-19 20:15:35 +0700

    Final Project

---
+++ script/popup.js
@@ -0,0 +1,12 @@
+const popup = document.querySelector(".popup");
+const close = document.querySelector(".close");
+
+window.onload = function () {
+  setTimeout(() => {
+    popup.style.display = "block";
+  }, 2000);
+};
+
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+});
